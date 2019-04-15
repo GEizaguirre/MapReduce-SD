6e6f5456f2eeaf50b24fca8ca23d3fab19e27af5
German Eizaguirre <germantelmo.eizaguirre@estudiants.urv>
2019-04-15 17:49:00 +0200

    german: Save de programa sin sistema de colas

--- orchestrator.py
+++ orchestrator.py
@@ -10,6 +10,7 @@ import ibm_botocore
 import ibm_cf_connection
 import configure_functions
 import configuration_paramethers
+import chunk
 
 ''' Default chunk size '''
 chunk_size=1
@@ -44,36 +45,58 @@ def main ():
         print ( " Processing... ")
     except ibm_botocore.exceptions.ClientError:
         print ( " Bucket ", bucket_name, " not found.")
-       
+     
+    chunk_size=15  
     dataset_size=658
-    dataset_end=dataset_size
-    ds_char='bytes='+str(dataset_end)+'-'+str(dataset_end)
-    
-    while (COS_session.get_object(bucket_name,
-                                dataset_name,
-                                extra_get_args={'Range': ds_char})).decode('utf-8').isalnum():
-        dataset_end+=1
-        ds_char='bytes='+str(dataset_end)+'-'+str(dataset_end)
-        
-    print ("Last position: ", dataset_end-1)
-    ds_range='bytes='+str(0)+'-'+str(dataset_end-1)
+    chunk_start=0
     
     map_dataset_info={'cos_config':{'endpoint':configuration_paramethers.cos_config['endpoint'],
-                      'secret_key':configuration_paramethers.cos_config['secret_key'],
-                      'access_key':configuration_paramethers.cos_config['access_key']},
-                      'bucket_name':bucket_name, 'dataset_name':dataset_name,
-                      'ds_range':ds_range}
-
-    #print (map_dataset.map_dataset(map_dataset_info))
-    
+                  'secret_key':configuration_paramethers.cos_config['secret_key'],
+                  'access_key':configuration_paramethers.cos_config['access_key']},
+                  'bucket_name':bucket_name, 'dataset_name':dataset_name,
+                  'ds_range':None}
     fn_session=ibm_cf_connection.CloudFunctions(functions_config)
-    configure_functions.configure_fn(fn_session)
-    #paramets={'name':'Juan'}
-    #print(fn_session.invoke("helloPython", paramets))
-    print(fn_session.invoke_with_result('mapDataset', map_dataset_info))
+    global_dict={'word_counting':0, 'word_count':dict()}
+    
+    while chunk_start<dataset_size:
+        
+        chunk_end=chunk_start+chunk_size
+        #print ("range: ", chunk_start, chunk_end)
+        if chunk_end>=dataset_size: chunk_end=dataset_size - 1
+        else:
+            ds_char='bytes='+str(chunk_end)+'-'+str(chunk_end)
+            
+            while (chunk_end<dataset_size) and (COS_session.get_object(bucket_name,
+                                        dataset_name,
+                                        extra_get_args={'Range': ds_char})).decode('utf-8').isalnum():
+                chunk_end+=1
+                ds_char='bytes='+str(chunk_end)+'-'+str(chunk_end)
+        
+            chunk_end-=1
+                
+        print ("range: ", chunk_start, chunk_end)
+        ds_range='bytes='+str(chunk_start)+'-'+str(chunk_end)
+        map_dataset_info['ds_range']=ds_range
+        chunk_dict=fn_session.invoke_with_result('mapDataset', map_dataset_info)
+        print (chunk_dict)
+        global_dict=mergeDict(global_dict, chunk_dict, lambda n1,n2: n1+n2)
+        # llamar a funcion
+        chunk_start=chunk_end+1
+        
+    print (global_dict)
+    print ("End of mapping")
  
 def show_help ():
     print (" MapReduce program. Needs a chunk size (optional) and a dataset name (compulsive).")
+    
+def mergeDict (map_result1, map_result2, mergingFunct= lambda x,y:x):
+    map_result1['word_counting']+=map_result2['word_counting']
+    for k,v in dict(map_result2['word_count']).items():
+        if k in map_result1['word_count']:
+            map_result1['word_count'][k] = mergingFunct(map_result1['word_count'][k], v)
+        else:
+            map_result1['word_count'][k] = v
+    return map_result1
        
 if __name__ == "__main__":
     main() 
