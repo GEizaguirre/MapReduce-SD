c30f27858639f4c6a5577f10390301038c5e93bb
German Eizaguirre <germantelmo.eizaguirre@estudiants.urv>
2019-04-16 13:26:32 +0200

    german: versión funcional 1.0

---
+++ save_result.py
@@ -0,0 +1,24 @@
+'''
+Created on 16 abr. 2019
+
+@author: German
+'''
+
+class ResultLog ():
+    
+    def __init__(self, cos_session):
+        self.dict = dict()
+        self.dict['counting_words'] = 0
+        self.dict['word_count'] = dict()
+        self.received_maps=0
+        self.sent_maps=0
+        self.COS_session = cos_session
+        
+    def increaseSent (self):
+        self.sent_maps+=1
+        
+    def increaseReceived (self):
+        self.received_maps+=1
+        
+    def reduceEnd (self):
+        return self.received_maps == self.sent_maps
